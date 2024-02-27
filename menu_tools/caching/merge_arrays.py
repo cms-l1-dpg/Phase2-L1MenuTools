b1328593a1e7a00a8a31c60f95e93bbcf821a5c4
Artur Lobanov <Artur.Lobanov@cern.ch>
2024-02-27 17:49:26 +0100

    Add pt cut in EG merge script

--- menu_tools/caching/merge_arrays.py
+++ menu_tools/caching/merge_arrays.py
@@ -1,25 +1,23 @@
 import awkward as ak
 
 version = "V32nano"
-sample = "Hgg"
-pattern = f"cache/f{version}/{version}_{sample}_%s.parquet"
+sample = "DYLL_M50"
+# sample = "Hgg"
+pattern = f"cache/{version}/{version}_{sample}_%s.parquet"
 objects = ["L1EGbarrel","L1EGendcap"]
 target_object = "L1EG"
 
 print(f"Reading files as {pattern} for {objects}")
-# arrs = [ak.from_parquet(pattern%obj) for obj in objects]
-## rename fields to be consistent
-# arrs [ak.zip({f.replace(obj):arr[f] for f in arr.fields}) for arr in arrs]
 arrs = []
 
 for obj in objects:
     arr = ak.from_parquet(pattern%obj)
     arr = ak.Array({f.replace(obj,target_object):arr[f] for f in arr.fields})
+    # apply 5 gev cut 
+    arr = arr[arr[f"{target_object}_pt"] > 5]
     print(obj, arr.fields)
     arrs.append(arr)
 
-# reprocess -> ak.unzip
-# arrs = [ak.Array(dict(zip([f.split("_")[-1] for f in arr.fields],ak.unzip(arr)))) for arr in arrs]
 print("Merging")
 merge_arr = ak.concatenate(arrs, axis=1)
 print(merge_arr)
