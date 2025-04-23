ab2fd76d147abcdac720be1310e376748b733621
Robert Ward <robert.james.ward@cern.ch>
2025-04-23 15:13:29 +0200

    Script with commented commands to rerun V44

---
+++ scripts/runV44.sh
@@ -0,0 +1,59 @@
+# === Initial Caching === #
+
+# cache_objects configs/V44nano/caching.yaml
+
+# === Object Performance === #
+
+## Electrons
+# object_performance configs/V44nano/object_performance/electron_matching.yaml
+# object_performance configs/V44nano/object_performance/electron_matching_eta.yaml
+# object_performance configs/V44nano/object_performance/electron_trigger.yaml
+
+## Photons
+# object_performance configs/V44nano/object_performance/photons_matching.yaml
+# object_performance configs/V44nano/object_performance/photons_matching_eta.yaml
+# object_performance configs/V44nano/object_performance/photons_trigger.yaml
+
+## Jets
+# object_performance configs/V44nano/object_performance/jets_matching.yaml
+# object_performance configs/V44nano/object_performance/jets_matching_eta.yaml
+# object_performance configs/V44nano/object_performance/jets_trigger.yaml
+
+## Taus
+# object_performance configs/V44nano/object_performance/tau_*.yaml
+
+## Sums
+# object_performance configs/V44nano/object_performance/met_ht_mht.yaml
+
+## MuonsTF
+# object_performance configs/V44nano/object_performance/muonTF_matching.yaml
+# object_performance configs/V44nano/object_performance/muonTF_matching_eta.yaml
+# object_performance configs/V44nano/object_performance/muonTF_trigger.yaml
+
+## Muons
+# object_performance configs/V44nano/object_performance/muonTF_matching.yaml
+# object_performance configs/V44nano/object_performance/muonTF_matching_eta.yaml
+# object_performance configs/V44nano/object_performance/muonTF_trigger.yaml
+
+
+# === Object Rates === #
+
+# rate_plots configs/V44nano/rate_plots/eg.yaml
+# rate_plots configs/V44nano/rate_plots/muons.yaml
+# rate_plots configs/V44nano/rate_plots/ht.yaml
+# rate_plots configs/V44nano/rate_plots/met.yaml
+# rate_plots configs/V44nano/rate_plots/jets.yaml
+# rate_plots configs/V44nano/rate_plots/taus.yaml
+# rate_plots configs/V44nano/rate_plots/tkmuons.yaml
+
+# === Menu Performance === #
+
+# rate_table configs/V44nano/rate_table/step1_cfg.yml
+# rate_table configs/V44nano/rate_table/step1_menu_cfg.yml
+
+# === Extras === #
+
+# object_performance configs/V44nano/object_performance/jets_matching_wBTag.yaml
+
+# rate_plots configs/V44nano/rate_plots/bjet.yaml
+# rate_plots configs/V44nano/rate_plots/disp_muons.yaml
