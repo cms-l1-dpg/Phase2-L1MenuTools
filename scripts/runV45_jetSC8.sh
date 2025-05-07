0bcc37f724672de45effa5e575a21f96862226e8
Robert Ward <robert.james.ward@cern.ch>
2025-05-07 07:21:06 +0200

    Menu testing

--- scripts/runV45_jetSC8.sh
+++ scripts/runV45_jetSC8.sh
@@ -1,65 +1,65 @@
-# === Initial Caching === #
+# # === Initial Caching === #
 
-cache_objects configs/V45nano_jetSC8/caching.yaml
-python3 menu_tools/caching/merge_arrays.py --version V45nano_jetSC8 --sample Hgg
-python3 menu_tools/caching/merge_arrays.py --version V45nano_jetSC8 --sample DYLL_M50
-python3 menu_tools/caching/merge_arrays.py --version V45nano_jetSC8 --sample MinBias
+# cache_objects configs/V45nano_jetSC8/caching.yaml
+# python3 menu_tools/caching/merge_arrays.py --version V45nano_jetSC8 --sample Hgg
+# python3 menu_tools/caching/merge_arrays.py --version V45nano_jetSC8 --sample DYLL_M50
+# python3 menu_tools/caching/merge_arrays.py --version V45nano_jetSC8 --sample MinBias
 
-# === Object Performance === #
+# # === Object Performance === #
 
-# Electrons
-object_performance configs/V45nano_jetSC8/object_performance/electron_matching.yaml
-object_performance configs/V45nano_jetSC8/object_performance/electron_matching_eta.yaml
-object_performance configs/V45nano_jetSC8/object_performance/electron_trigger.yaml
+# # Electrons
+# object_performance configs/V45nano_jetSC8/object_performance/electron_matching.yaml
+# object_performance configs/V45nano_jetSC8/object_performance/electron_matching_eta.yaml
+# object_performance configs/V45nano_jetSC8/object_performance/electron_trigger.yaml
 
-# Photons
-object_performance configs/V45nano_jetSC8/object_performance/photons_matching.yaml
-object_performance configs/V45nano_jetSC8/object_performance/photons_matching_eta.yaml
-object_performance configs/V45nano_jetSC8/object_performance/photons_trigger.yaml
+# # Photons
+# object_performance configs/V45nano_jetSC8/object_performance/photons_matching.yaml
+# object_performance configs/V45nano_jetSC8/object_performance/photons_matching_eta.yaml
+# object_performance configs/V45nano_jetSC8/object_performance/photons_trigger.yaml
 
-# Jets
-object_performance configs/V45nano_jetSC8/object_performance/jets_matching.yaml
-object_performance configs/V45nano_jetSC8/object_performance/jets_matching_eta.yaml
-object_performance configs/V45nano_jetSC8/object_performance/jets_trigger.yaml
+# # Jets
+# object_performance configs/V45nano_jetSC8/object_performance/jets_matching.yaml
+# object_performance configs/V45nano_jetSC8/object_performance/jets_matching_eta.yaml
+# object_performance configs/V45nano_jetSC8/object_performance/jets_trigger.yaml
 
-# Taus
-object_performance configs/V45nano_jetSC8/object_performance/tau_matching.yaml
-object_performance configs/V45nano_jetSC8/object_performance/tau_matching_eta.yaml
-object_performance configs/V45nano_jetSC8/object_performance/tau_matching_highPt.yaml
-object_performance configs/V45nano_jetSC8/object_performance/tau_trigger.yaml
+# # Taus
+# object_performance configs/V45nano_jetSC8/object_performance/tau_matching.yaml
+# object_performance configs/V45nano_jetSC8/object_performance/tau_matching_eta.yaml
+# object_performance configs/V45nano_jetSC8/object_performance/tau_matching_highPt.yaml
+# object_performance configs/V45nano_jetSC8/object_performance/tau_trigger.yaml
 
-# Sums
-object_performance configs/V45nano_jetSC8/object_performance/met_ht_mht.yaml
+# # Sums
+# object_performance configs/V45nano_jetSC8/object_performance/met_ht_mht.yaml
 
-# Muons
-object_performance configs/V45nano_jetSC8/object_performance/muon_matching.yaml
-object_performance configs/V45nano_jetSC8/object_performance/muon_matching_eta.yaml
-object_performance configs/V45nano_jetSC8/object_performance/muon_trigger.yaml
+# # Muons
+# object_performance configs/V45nano_jetSC8/object_performance/muon_matching.yaml
+# object_performance configs/V45nano_jetSC8/object_performance/muon_matching_eta.yaml
+# object_performance configs/V45nano_jetSC8/object_performance/muon_trigger.yaml
 
-# TkMuons
-object_performance configs/V45nano_jetSC8/object_performance/tkmuon_matching.yaml
-object_performance configs/V45nano_jetSC8/object_performance/tkmuon_matching_eta.yaml
-object_performance configs/V45nano_jetSC8/object_performance/tkmuon_trigger.yaml
+# # TkMuons
+# object_performance configs/V45nano_jetSC8/object_performance/tkmuon_matching.yaml
+# object_performance configs/V45nano_jetSC8/object_performance/tkmuon_matching_eta.yaml
+# object_performance configs/V45nano_jetSC8/object_performance/tkmuon_trigger.yaml
 
-# MuonsTF
-object_performance configs/V45nano_jetSC8/object_performance/muonTF_matching.yaml
-object_performance configs/V45nano_jetSC8/object_performance/muonTF_matching_eta.yaml
-object_performance configs/V45nano_jetSC8/object_performance/muonTF_trigger.yaml
+# # MuonsTF
+# object_performance configs/V45nano_jetSC8/object_performance/muonTF_matching.yaml
+# object_performance configs/V45nano_jetSC8/object_performance/muonTF_matching_eta.yaml
+# object_performance configs/V45nano_jetSC8/object_performance/muonTF_trigger.yaml
 
-# === Object Rates === #
+# # === Object Rates === #
 
-rate_plots configs/V45nano_jetSC8/rate_plots/eg.yaml
-rate_plots configs/V45nano_jetSC8/rate_plots/muons.yaml
-rate_plots configs/V45nano_jetSC8/rate_plots/ht.yaml
-rate_plots configs/V45nano_jetSC8/rate_plots/met.yaml
-rate_plots configs/V45nano_jetSC8/rate_plots/jets.yaml
-rate_plots configs/V45nano_jetSC8/rate_plots/taus.yaml
-rate_plots configs/V45nano_jetSC8/rate_plots/tkmuons.yaml
+# rate_plots configs/V45nano_jetSC8/rate_plots/eg.yaml
+# rate_plots configs/V45nano_jetSC8/rate_plots/muons.yaml
+# rate_plots configs/V45nano_jetSC8/rate_plots/ht.yaml
+# rate_plots configs/V45nano_jetSC8/rate_plots/met.yaml
+# rate_plots configs/V45nano_jetSC8/rate_plots/jets.yaml
+# rate_plots configs/V45nano_jetSC8/rate_plots/taus.yaml
+# rate_plots configs/V45nano_jetSC8/rate_plots/tkmuons.yaml
 
 # === Menu Performance === #
 
 rate_table configs/V45nano_jetSC8/rate_table/step1_cfg.yml
-rate_table configs/V45nano_jetSC8/rate_table/step1_menu_cfg.yml
+# rate_table configs/V45nano_jetSC8/rate_table/step1_menu_cfg.yml
 
 # === Extras === #
 
