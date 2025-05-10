1a7cb0133e8e5ad439718cad68710fcde054ee6e
Robert Ward <robert.james.ward@cern.ch>
2025-05-10 10:23:03 +0200

    First bisecting configs

---
+++ scripts/setupConfig.sh
@@ -0,0 +1,8 @@
+INPUT=$1
+OUTPUT=$2
+
+# cp -r configs/$INPUT configs/$OUTPUT
+# grep -rl "$INPUT" configs/$OUTPUT | xargs sed -i "s/$INPUT/$OUTPUT/g"
+
+cp -r configs/V45nano_$INPUT configs/V45nano_$OUTPUT
+grep -rl "$INPUT" configs/V45nano_$OUTPUT | xargs sed -i "s/$INPUT/$OUTPUT/g"
