084d50fea17ec39bdc285b86062b3a39f93cb43e
Robert Ward <robert.james.ward@cern.ch>
2025-05-10 10:24:58 +0200

    First bisecting comparisons

--- menu_tools/utils/runComparisons.sh
+++ menu_tools/utils/runComparisons.sh
@@ -2,7 +2,10 @@
 # python compare_json-wNano.py --v1 V45nano --v0 V45nano_jetSC8
 # python compare_json-wNano.py --v1 V44nano --v0 V45nano_l1Track
 # python compare_json-wNano.py --v1 V45nano --v0 V45nano_l1Track
-python compare_json-wNano.py --v1 V44nano --v0 V45nano_firstTrain
-python compare_json-wNano.py --v1 V45nano --v0 V45nano_firstTrain
-python compare_json-wNano.py --v1 V44nano --v0 V45nano_jetSC4NG
-python compare_json-wNano.py --v1 V45nano --v0 V45nano_jetSC4NG
+# python compare_json-wNano.py --v1 V44nano --v0 V45nano_firstTrain
+# python compare_json-wNano.py --v1 V45nano --v0 V45nano_firstTrain
+# python compare_json-wNano.py --v1 V44nano --v0 V45nano_jetSC4NG
+# python compare_json-wNano.py --v1 V45nano --v0 V45nano_jetSC4NG
+python compare_json-wNano.py --v1 V44nano --v0 V45nano_142pre4
+python compare_json-wNano.py --v1 V45nano_142pre4 --v0 V45nano_150pre3
+python compare_json-wNano.py --v1 V45nano_150pre3 --v0 V45nano
