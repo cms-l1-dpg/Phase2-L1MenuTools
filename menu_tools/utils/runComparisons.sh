cb13ad55ff207d2c01cf68a580197f84e5b98a5a
Robert Ward <robert.james.ward@cern.ch>
2025-05-30 14:03:00 +0200

    Latest comparisons plots

--- menu_tools/utils/runComparisons.sh
+++ menu_tools/utils/runComparisons.sh
@@ -6,6 +6,43 @@
 # python compare_json-wNano.py --v1 V45nano --v0 V45nano_firstTrain
 # python compare_json-wNano.py --v1 V44nano --v0 V45nano_jetSC4NG
 # python compare_json-wNano.py --v1 V45nano --v0 V45nano_jetSC4NG
-python compare_json-wNano.py --v1 V44nano --v0 V45nano_142pre4
-python compare_json-wNano.py --v1 V45nano_142pre4 --v0 V45nano_150pre3
-python compare_json-wNano.py --v1 V45nano_150pre3 --v0 V45nano
+
+# Bisecting
+# python compare_json-wNano.py --v1 V44nano --v0 V45nano_142pre4
+# python compare_json-wNano.py --v1 V45nano_142pre4 --v0 V45nano_150pre3
+# python compare_json-wNano.py --v1 V45nano_150pre3 --v0 V45nano
+
+# Second train PR
+# python compare_json-wNano.py --v1 V44nano --v0 V45nano_L1EG
+# python compare_json-wNano.py --v1 V45nano --v0 V45nano_L1EG
+
+
+# Bisecting
+# python compare_json-wNano.py --v1 V44nano --v0 V45nano_142pre2
+# python compare_json-wNano.py --v1 V45nano_142pre2 --v0 V45nano_142pre3
+# python compare_json-wNano.py --v1 V45nano_142pre3 --v0 V45nano_142pre4
+# python compare_json-wNano.py --v1 V45nano_142pre4 --v0 V45nano_150pre1
+# python compare_json-wNano.py --v1 V45nano_150pre1 --v0 V45nano_150pre2
+# python compare_json-wNano.py --v1 V45nano_150pre2 --v0 V45nano_150pre3
+# python compare_json-wNano.py --v1 V45nano_150pre3 --v0 V45nano_151pre1
+# python compare_json-wNano.py --v1 V45nano_151pre1 --v0 V45nano
+# python compare_json-wNano.py --v1 V45nano --v0 V45nano_nanoSC4NG
+# python compare_json-wNano.py --v1 V45nano_nanoSC4NG --v0 V45nano_L1EG
+
+python compare_json-wNano.py --v1 V45nano --v0 V45nano_151pre3
+
+
+# # Muon RPC Geometry change
+# python compare_json-wNano.py --v1 V45nano_150pre2 --v0 V45nano_150pre2_RPC
+# python compare_json-wNano.py --v1 V45nano_150pre2_RPC --v0 V45nano_150pre3
+
+# L1EG mega commit
+# python compare_json-wNano.py --v1 V45nano --v0 V45nano_noL1EG
+# python compare_json-wNano.py --v1 V45nano_noL1EG --v0 V45nano_L1EGupdate1
+# python compare_json-wNano.py --v1 V45nano_noL1EG --v0 V45nano_L1EG
+# python compare_json-wNano.py --v1 V45nano_L1EG --v0 V45nano_L1EGupdate1
+
+# # L1Track differences
+# python compare_json-wNano.py --v1 V45nano_142pre3 --v0 V45nano_142pre3_L1Track
+# python compare_json-wNano.py --v1 V45nano_142pre3_L1Track --v0 V45nano_142pre4
+
