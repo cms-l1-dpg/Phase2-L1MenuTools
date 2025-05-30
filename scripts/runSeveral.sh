dee0cf3c5540bbf98d22504825b7ea3b84cb41a9
Robert Ward <robert.james.ward@cern.ch>
2025-05-30 14:03:55 +0200

    Update script to run parallel jobs

--- scripts/runSeveral.sh
+++ scripts/runSeveral.sh
@@ -1,5 +1,18 @@
-VERSIONS="L1EG 151pre1 150pre3 150pre2 150pre1 142pre3 142pre2"
+# VERSIONS="L1EG 151pre1 150pre2 150pre1 142pre3 142pre2"
+# VERSIONS="noL1EG L1EG L1EGupdate1"
+VERSIONS="L1EGupdate1"
+# VERSIONS="151pre3"
+REVISION="250528"
+
+MAX_JOBS=10
 
 for VERSION in $VERSIONS; do
-    source scripts/runGeneric.sh V45nano_$VERSION |& tee logs/V45nano_${VERSION}_taus.log
+    while [ $(jobs -p | wc -l) -ge $MAX_JOBS ]; do
+        sleep 3
+    done
+    (
+	source scripts/runGeneric.sh V45nano_$VERSION &> logs/V45nano_${VERSION}_${REVISION}.log
+    ) &
 done
+
+wait
