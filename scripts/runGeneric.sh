09ebda736b37180b81c7f90fe8657579348333bc
Robert Ward <robert.james.ward@cern.ch>
2025-06-01 07:21:51 +0200

    Parallelise config plotting on within-version level

--- scripts/runGeneric.sh
+++ scripts/runGeneric.sh
@@ -1,72 +1,97 @@
 VERSION=$1
+REVISION=$2
+MAX_JOBS=5
 
+run_when_ready() {
+    while [ $(jobs -p | wc -l) -ge $MAX_JOBS ]; do
+        sleep 3
+    done
+    # Extract a meaningful identifier from the config path
+    local filename=$(basename "$2")  # assumes config is 2nd argument
+    local logname=${filename%.*}  # removes file extension
+    echo Submitting $logname
+    "$@" &> logs/${VERSION}_${REVISION}/${VERSION}_${logname}.log &
+}
+
+mkdir -p logs/${VERSION}_${REVISION}
 # === Initial Caching === #
 
-cache_objects configs/$VERSION/caching.yaml
-python3 menu_tools/caching/merge_arrays.py --version $VERSION --sample Hgg
-python3 menu_tools/caching/merge_arrays.py --version $VERSION --sample DYLL_M50
-python3 menu_tools/caching/merge_arrays.py --version $VERSION --sample MinBias
+echo "Running caching"
+
+(
+    cache_objects configs/$VERSION/caching.yaml &> logs/${VERSION}_${REVISION}/${VERSION}_caching.log
+    python3 menu_tools/caching/merge_arrays.py --version $VERSION --sample Hgg &>> logs/${VERSION}_${REVISION}/${VERSION}_caching.log
+    python3 menu_tools/caching/merge_arrays.py --version $VERSION --sample DYLL_M50 &>> logs/${VERSION}_${REVISION}/${VERSION}_caching.log
+    python3 menu_tools/caching/merge_arrays.py --version $VERSION --sample MinBias &>> logs/${VERSION}_${REVISION}/${VERSION}_caching.log
+) &
+
+echo "Waiting for caching"
+wait
+echo "Caching complete"
 
 # === Object Performance (Matching) === #
 
 # Electrons
-object_performance configs/$VERSION/object_performance/electron_matching.yaml
-object_performance configs/$VERSION/object_performance/electron_matching_eta.yaml
+run_when_ready object_performance configs/$VERSION/object_performance/electron_matching.yaml
+run_when_ready object_performance configs/$VERSION/object_performance/electron_matching_eta.yaml
 
 # Photons
-object_performance configs/$VERSION/object_performance/photons_matching.yaml
-object_performance configs/$VERSION/object_performance/photons_matching_eta.yaml
+run_when_ready object_performance configs/$VERSION/object_performance/photons_matching.yaml
+run_when_ready object_performance configs/$VERSION/object_performance/photons_matching_eta.yaml
 
 # Jets
-object_performance configs/$VERSION/object_performance/jets_matching.yaml
-object_performance configs/$VERSION/object_performance/jets_matching_eta.yaml
-object_performance configs/$VERSION/object_performance/jets_matching_wBTag.yaml
+run_when_ready object_performance configs/$VERSION/object_performance/jets_matching.yaml
+run_when_ready object_performance configs/$VERSION/object_performance/jets_matching_eta.yaml
+run_when_ready object_performance configs/$VERSION/object_performance/jets_matching_wBTag.yaml
 
 # Muons
-object_performance configs/$VERSION/object_performance/muon_matching.yaml
-object_performance configs/$VERSION/object_performance/muon_matching_eta.yaml
+run_when_ready object_performance configs/$VERSION/object_performance/muon_matching.yaml
+run_when_ready object_performance configs/$VERSION/object_performance/muon_matching_eta.yaml
 
 # TkMuons
-object_performance configs/$VERSION/object_performance/tkmuon_matching.yaml
-object_performance configs/$VERSION/object_performance/tkmuon_matching_eta.yaml
+run_when_ready object_performance configs/$VERSION/object_performance/tkmuon_matching.yaml
+run_when_ready object_performance configs/$VERSION/object_performance/tkmuon_matching_eta.yaml
 
 # MuonsTF
-object_performance configs/$VERSION/object_performance/muonTF_matching.yaml
-object_performance configs/$VERSION/object_performance/muonTF_matching_eta.yaml
+run_when_ready object_performance configs/$VERSION/object_performance/muonTF_matching.yaml
+run_when_ready object_performance configs/$VERSION/object_performance/muonTF_matching_eta.yaml
 
 # Taus
-object_performance configs/$VERSION/object_performance/tau_matching.yaml
-object_performance configs/$VERSION/object_performance/tau_matching_eta.yaml
-object_performance configs/$VERSION/object_performance/tau_matching_highPt.yaml
+run_when_ready object_performance configs/$VERSION/object_performance/tau_matching.yaml
+run_when_ready object_performance configs/$VERSION/object_performance/tau_matching_eta.yaml
+run_when_ready object_performance configs/$VERSION/object_performance/tau_matching_highPt.yaml
 
 # === Object Performance (Triggers/Scalings) === #
 
 # Sums (all in one!)
-object_performance configs/$VERSION/object_performance/met_ht_mht.yaml
+run_when_ready object_performance configs/$VERSION/object_performance/met_ht_mht.yaml
 
-object_performance configs/$VERSION/object_performance/electron_trigger.yaml
-object_performance configs/$VERSION/object_performance/photons_trigger.yaml
-object_performance configs/$VERSION/object_performance/jets_trigger.yaml
-object_performance configs/$VERSION/object_performance/muon_trigger.yaml
-object_performance configs/$VERSION/object_performance/tkmuon_trigger.yaml
-object_performance configs/$VERSION/object_performance/muonTF_trigger.yaml
-object_performance configs/$VERSION/object_performance/tau_trigger.yaml
+run_when_ready object_performance configs/$VERSION/object_performance/electron_trigger.yaml
+run_when_ready object_performance configs/$VERSION/object_performance/photons_trigger.yaml
+run_when_ready object_performance configs/$VERSION/object_performance/jets_trigger.yaml
+run_when_ready object_performance configs/$VERSION/object_performance/muon_trigger.yaml
+run_when_ready object_performance configs/$VERSION/object_performance/tkmuon_trigger.yaml
+run_when_ready object_performance configs/$VERSION/object_performance/muonTF_trigger.yaml
+run_when_ready object_performance configs/$VERSION/object_performance/tau_trigger.yaml
 
+echo "Waiting for scalings"
+wait
+echo "Scalings complete"
 
 # === Object Rates === #
 
-rate_plots configs/$VERSION/rate_plots/eg.yaml
-rate_plots configs/$VERSION/rate_plots/muons.yaml
-rate_plots configs/$VERSION/rate_plots/tkmuons.yaml
-rate_plots configs/$VERSION/rate_plots/ht.yaml
-rate_plots configs/$VERSION/rate_plots/met.yaml
-rate_plots configs/$VERSION/rate_plots/jets.yaml
-rate_plots configs/$VERSION/rate_plots/taus.yaml
+run_when_ready rate_plots configs/$VERSION/rate_plots/eg.yaml
+run_when_ready rate_plots configs/$VERSION/rate_plots/muons.yaml
+run_when_ready rate_plots configs/$VERSION/rate_plots/tkmuons.yaml
+run_when_ready rate_plots configs/$VERSION/rate_plots/ht.yaml
+run_when_ready rate_plots configs/$VERSION/rate_plots/met.yaml
+run_when_ready rate_plots configs/$VERSION/rate_plots/jets.yaml
+run_when_ready rate_plots configs/$VERSION/rate_plots/taus.yaml
+run_when_ready rate_plots configs/$VERSION/rate_plots/bjet.yaml
+run_when_ready rate_plots configs/$VERSION/rate_plots/disp_muons.yaml
 
 # === Menu Performance === #
 
-rate_table configs/$VERSION/rate_table/step1_cfg.yml
+run_when_ready rate_table configs/$VERSION/rate_table/step1_cfg.yml
 
-# === Extras === #
-rate_plots configs/$VERSION/rate_plots/bjet.yaml
-rate_plots configs/$VERSION/rate_plots/disp_muons.yaml
+echo "Plots and tables finished!"
--- scripts/runSeveral.sh
+++ scripts/runSeveral.sh
@@ -1,18 +1,21 @@
 # VERSIONS="L1EG 151pre1 150pre2 150pre1 142pre3 142pre2"
 # VERSIONS="noL1EG L1EG L1EGupdate1"
-VERSIONS="L1EGupdate1"
+# VERSIONS="L1EGupdate1"
 # VERSIONS="151pre3"
-REVISION="250528"
-
-MAX_JOBS=10
+# VERSIONS="151X_D110 151X_D116 151X_D121 151pre3_D110Fix"
+# VERSIONS="151pre3_D110Fix"
+VERSIONS="151pre3_SC8Nano"
+REVISION="250530"
+MAX_JOBS=3
 
 for VERSION in $VERSIONS; do
     while [ $(jobs -p | wc -l) -ge $MAX_JOBS ]; do
         sleep 3
     done
-    (
-	source scripts/runGeneric.sh V45nano_$VERSION &> logs/V45nano_${VERSION}_${REVISION}.log
-    ) &
+    echo "Submitting ${VERSION}_${REVISION}"
+    mkdir -p logs/V45nano_${VERSION}_${REVISION}
+    source scripts/runGeneric.sh V45nano_$VERSION $REVISION &> logs/V45nano_${VERSION}_${REVISION}/V45nano_${VERSION}.log &
 done
 
+echo "All jobs submitted, but waiting as some are still running! Check on progress in another tab."
 wait
