a1600eb036340d4fc2396801d1ab77241c531b6c
Robert Ward <robert.james.ward@cern.ch>
2025-06-05 21:58:46 +0200

    Change so V45nano is no longer assumed

--- scripts/runSeveral.sh
+++ scripts/runSeveral.sh
@@ -6,17 +6,19 @@
 # VERSIONS="151pre3_D110Fix"
 # VERSIONS="151pre3_SC8Nano"
 # VERSIONS="L1EGupdate2"
-VERSIONS="L1EGupdate1p5"
-REVISION="250601"
-MAX_JOBS=1
+# NOTE: above versions assumed V45nano in the name
+
+VERSIONS="V46nano_L1EGupdate2 V46nano_151pre3"
+REVISION="250605"
+MAX_JOBS=2
 
 for VERSION in $VERSIONS; do
     while [ $(jobs -p | wc -l) -ge $MAX_JOBS ]; do
         sleep 3
     done
     echo "Submitting ${VERSION}_${REVISION}"
-    mkdir -p logs/V45nano_${VERSION}_${REVISION}
-    source scripts/runGeneric.sh V45nano_$VERSION $REVISION &> logs/V45nano_${VERSION}_${REVISION}/V45nano_${VERSION}.log &
+    mkdir -p logs/${VERSION}_${REVISION}
+    source scripts/runGeneric.sh $VERSION $REVISION &> logs/${VERSION}_${REVISION}/${VERSION}.log &
 done
 
 echo "All jobs submitted, but waiting as some are still running! Check on progress in another tab."
