d41b4704fe979f37b47610b62b5670235af9c74b
Robert Ward <robert.james.ward@cern.ch>
2025-06-05 21:57:33 +0200

    Change to no longer assuming V45nano

--- scripts/setupConfig.sh
+++ scripts/setupConfig.sh
@@ -1,8 +1,8 @@
 INPUT=$1
 OUTPUT=$2
 
-# cp -r configs/$INPUT configs/$OUTPUT
-# grep -rl "$INPUT" configs/$OUTPUT | xargs sed -i "s/$INPUT/$OUTPUT/g"
+cp -r configs/$INPUT configs/$OUTPUT
+grep -rl "$INPUT" configs/$OUTPUT | xargs sed -i "s/$INPUT/$OUTPUT/g"
 
-cp -r configs/V45nano_$INPUT configs/V45nano_$OUTPUT
-grep -rl "$INPUT" configs/V45nano_$OUTPUT | xargs sed -i "s/_$INPUT/_$OUTPUT/g"
+# cp -r configs/V45nano_$INPUT configs/V45nano_$OUTPUT
+# grep -rl "$INPUT" configs/V45nano_$OUTPUT | xargs sed -i "s/_$INPUT/_$OUTPUT/g"
