56abacb1ec067044a15dca2699f33f1f7619e4b9
Robert Ward <robert.james.ward@cern.ch>
2025-06-11 11:49:17 +0200

    Add in missing sc8 config for HF/HGC step2 splits

--- scripts/copyStep2ForwardJet.sh
+++ scripts/copyStep2ForwardJet.sh
@@ -4,6 +4,7 @@ RERUN=$2
 cp configs/V49nano_151pre3/rate_table/step2_cfg.yml configs/$VERSION/rate_table/.
 cp configs/V49nano_151pre3/rate_table/step2_menu_cfg.yml configs/$VERSION/rate_table/.
 cp configs/V49nano_151pre3/object_performance/jets_ext_trigger.yaml configs/$VERSION/object_performance/.
+cp configs/V49nano_151pre3/object_performance/jets_sc8_trigger.yaml configs/$VERSION/object_performance/.
 cp configs/V49nano_151pre3/rate_plots/jets_sc8.yaml configs/$VERSION/rate_plots/.
 
 grep -rl "V49nano_151pre3" configs/$VERSION/object_performance | xargs sed -i "s/V49nano_151pre3/$VERSION/g"
