643e940fb3ea3bf8e01cfce1ddb2c581766055eb
Robert Ward <robert.james.ward@cern.ch>
2025-06-11 11:06:06 +0200

    Add flag to only rerun step2 if desired

--- scripts/copyStep2.sh
+++ scripts/copyStep2.sh
@@ -1,4 +1,5 @@
 VERSION=$1
+RERUN=$2
 
 cp configs/V44nano/rate_table/step2_cfg.yml configs/$VERSION/rate_table/.
 cp configs/V44nano/rate_table/step2_menu_cfg.yml configs/$VERSION/rate_table/.
@@ -10,8 +11,10 @@ grep -rl "V44nano" configs/$VERSION/rate_plots | xargs sed -i "s/V44nano/$VERSIO
 grep -rl "V44nano" configs/$VERSION/rate_table | xargs sed -i "s/V44nano/$VERSION/g"
 grep -rl "v44_" configs/$VERSION/rate_table | xargs sed -i "s/v44_/v45_/g"
 
-object_performance configs/$VERSION/object_performance/jets_sc8_trigger.yaml
-object_performance configs/$VERSION/object_performance/jets_ext_trigger.yaml
-rate_table configs/$VERSION/rate_table/step2_cfg.yml
-rate_plots configs/$VERSION/rate_plots/jets_sc8.yaml
+if [[ $RERUN == "TRUE" ]]; then
+    object_performance configs/$VERSION/object_performance/jets_sc8_trigger.yaml
+    object_performance configs/$VERSION/object_performance/jets_ext_trigger.yaml
+    rate_table configs/$VERSION/rate_table/step2_cfg.yml
+    rate_plots configs/$VERSION/rate_plots/jets_sc8.yaml
+fi
 
