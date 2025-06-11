bac6d1dfc2e82829fe88f92dc9cc6fab43b143c9
Robert Ward <robert.james.ward@cern.ch>
2025-06-11 09:55:25 +0200

    Only rerun eg rates and menu if requested

--- scripts/swapToElectronScalings.sh
+++ scripts/swapToElectronScalings.sh
@@ -1,4 +1,5 @@
 VERSION=$1
+RERUN=$2
 
 if [ -f outputs/$VERSION/object_performance/scalings/ElectronsTriggerBarrel_L1EG:default:barrel.yaml ]; then
    echo "Found separate electron L1EG scalings, now updating"
@@ -10,9 +11,11 @@ if [ -f outputs/$VERSION/object_performance/scalings/ElectronsTriggerBarrel_L1EG
    mv outputs/$VERSION/object_performance/scalings/ElectronsTriggerBarrel_L1EG:default:barrel.yaml outputs/$VERSION/object_performance/scalings/L1EG:default:barrel.yaml
    mv outputs/$VERSION/object_performance/scalings/ElectronsTriggerEndcap_L1EG:default:endcap.yaml outputs/$VERSION/object_performance/scalings/L1EG:default:endcap.yaml
 
-   # rerun EG rates and menu
-   rate_plots configs/$VERSION/rate_plots/eg.yaml
-   rate_table configs/$VERSION/rate_table/step1_cfg.yml
+   if [[ $RERUN == "TRUE" ]]; then
+       # rerun EG rates and menu
+       rate_plots configs/$VERSION/rate_plots/eg.yaml
+       rate_table configs/$VERSION/rate_table/step1_cfg.yml
+   fi
 else
    echo "Separate electron L1EG scalings don't exist, please check"
 fi
