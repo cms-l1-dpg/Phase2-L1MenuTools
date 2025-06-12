3e72f537d14e037740487323f5b123cfb43fc049
Robert Ward <robert.james.ward@cern.ch>
2025-06-12 20:18:47 +0200

    Have cache symlinker use config versions directly

--- scripts/symlinkCache.sh
+++ scripts/symlinkCache.sh
@@ -2,13 +2,13 @@
 
 INPUT=$1
 OUTPUT=$2
-VOLD=V45nano
-VNEW=V49nano
+# VOLD=V45nano
+# VNEW=V49nano
 
 mkdir -p cache/"$OUTPUT"
 
 for file in cache/"$INPUT"/*.parquet; do
     basename=$(basename "$file")
-    newname=${basename/$VOLD/$VNEW}
+    newname=${basename/$INPUT/$OUTPUT}
     ln -s "$(realpath "$file")" "cache/$OUTPUT/$newname"
 done
