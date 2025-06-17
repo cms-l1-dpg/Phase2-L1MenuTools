c32812f7bf5d2aa1883b29451213a8ac6fcd66f8
Robert Ward <robert.james.ward@cern.ch>
2025-06-17 12:06:09 +0200

    Copy caching config from source version

--- scripts/symlinkCache.sh
+++ scripts/symlinkCache.sh
@@ -2,6 +2,7 @@
 
 INPUT=$1
 OUTPUT=$2
+UPDATEYAML=TRUE
 # VOLD=V45nano
 # VNEW=V49nano
 
@@ -12,3 +13,10 @@ for file in cache/"$INPUT"/*.parquet; do
     newname=${basename/$INPUT/$OUTPUT}
     ln -s "$(realpath "$file")" "cache/$OUTPUT/$newname"
 done
+
+if [[ $UPDATEYAML == "TRUE" ]]; then
+    echo "Updating caching yaml"
+    cp configs/$INPUT/caching.yaml configs/$OUTPUT/.
+    grep -rl "$INPUT" configs/$OUTPUT/caching.yaml | xargs sed -i "s/$INPUT/$OUTPUT/g"    
+fi
+# Update caching.yaml
