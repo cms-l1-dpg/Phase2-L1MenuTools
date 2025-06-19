409e4e65180f062fbcb7ebaf80c9ae6f91d476a2
Robert Ward <robert.james.ward@cern.ch>
2025-06-19 14:33:11 +0200

    Run on vtx options for 151pre3

--- scripts/runSeveral.sh
+++ scripts/runSeveral.sh
@@ -13,8 +13,9 @@
 # VERSIONS="V49nano_151pre3_E2ENNVtx V49nano_151pre3_E2ENNVtxOff"
 # VERSIONS="V49nano_AR24"
 # VERSIONS=$1
-VERSIONS="V49nano_AR25_VtxFindOff V49nano_AR25_VtxOnlyAssoc V49nano_AR25_VtxBothOn V49nano_151pre3_L1EGupdate4 V49nano_151pre3_VtxOnlyFind V49nano_151pre3_VtxOnlyAssoc V49nano_151pre3_DispVtx"
-REVISION="250617"
+# VERSIONS="V49nano_AR25_VtxFindOff V49nano_AR25_VtxOnlyAssoc V49nano_AR25_VtxBothOn V49nano_151pre3_L1EGupdate4 V49nano_151pre3_VtxOnlyFind V49nano_151pre3_VtxOnlyAssoc V49nano_151pre3_DispVtx"
+VERSIONS="V49nano_151pre3_VtxOnlyFind V49nano_151pre3_VtxOnlyAssoc V49nano_151pre3_DispVtx"
+REVISION="250619"
 MAX_JOBS=3
 
 # run_when_ready() {
