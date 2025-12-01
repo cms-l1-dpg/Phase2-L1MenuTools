205b7a33552fbd418ed65664dbd8ca1fb6306ace
Robert Ward <robert.james.ward@cern.ch>
2025-12-01 16:57:29 +0100

    Add default PU value to scalings plotter too

--- menu_tools/object_performance/plotter.py
+++ menu_tools/object_performance/plotter.py
@@ -381,6 +381,7 @@ class ScalingPlotter(Plotter):
         scaling_pct: float,
         version: str,
         params: dict[str, np.ndarray],
+        pu_value: int = 200,
     ):
         self.plot_name = plot_name
         self.plot_config = plot_config
@@ -388,6 +389,7 @@ class ScalingPlotter(Plotter):
         self.params = params
         self.version = version
         self.scaling_pct = scaling_pct
+        self.pu_value = pu_value
 
     def _params_to_func_str(self, obj_key: str):
         a = round(self.params[obj_key][0], 3)
