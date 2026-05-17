77722bcd07679396b90b5796718913363818ed70
Robert Ward <robert.james.ward@cern.ch>
2026-05-17 13:05:26 +0200

    Start updating rename to be able to be run from top directory of repo

--- scripts/rename.sh
+++ scripts/rename.sh
@@ -1,16 +1,29 @@
-OLDVERSION=V38nano_DT12x
-NEWVERSION=V38nano_DT12x_Oct24
+# OLDVERSION=V38nano_DT12x
+# NEWVERSION=V38nano_DT12x_Oct24
+OLDVERSION=$1
+NEWVERSION=$2
 
-cd $NEWVERSION
+# HEADDIR=cache
+HEADDIR=outputs
 
-# Filenames
-for file in $(find -name *$OLDVERSION*); do
-    echo Oldname: $file
-    echo Newname: ${file/$OLDVERSION/$NEWVERSION}
-    mv $file ${file/$OLDVERSION/$NEWVERSION}
-done
 
-# Text within files
-grep -rl $OLDVERSION --exclude rename.sh | xargs sed -i "s/$OLDVERSION/$NEWVERSION/g"
+if [ -d "$HEADDIR/$OLDVERSION" ]; then
+    mv $HEADDIR/$OLDVERSION $HEADDIR/$NEWVERSION
 
-cd -
+    cd $HEADDIR/$NEWVERSION
+
+    # Filenames
+    for file in $(find -name "*$OLDVERSION*"); do
+	echo Oldname: $file
+	echo Newname: ${file/$OLDVERSION/$NEWVERSION}
+	mv $file ${file/$OLDVERSION/$NEWVERSION}
+    done
+
+    echo "Files renamed, now checking in files"
+    # Text within files
+    grep -rl $OLDVERSION | xargs sed -i "s/$OLDVERSION/$NEWVERSION/g"
+
+    cd -
+else
+    echo "ERROR: Old version $HEADDIR/$OLDVERSION doesnt exist, please check"
+fi
