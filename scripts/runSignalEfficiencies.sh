771fe27650ba8575f05b143f439e1bea4326dba1
Robert Ward <robert.james.ward@cern.ch>
2026-09-16 01:06:21 +0200

    Add timer to signal efficiencies script

--- scripts/runSignalEfficiencies.sh
+++ scripts/runSignalEfficiencies.sh
@@ -5,7 +5,7 @@ VERSION="V50nano_170pre3"
 SIGNALS="DYLL_M50 Hgg HHTo4B HHTo2B2Tau TT VBFHToTauTau VBFHToBB VBFHToInv"
 
 for signal in $SIGNALS; do
-    rate_table configs/V50nano/rate_table/step${MENU}_cfg.yml \
-        --version $VERSION --signal $signal \
-        | tee logs/${VERSION}_step${MENU}_${signal}_signalEff.log
+  { time rate_table configs/V50nano/rate_table/step${MENU}_cfg.yml \
+        --version $VERSION --signal $signal ; } \
+        |& tee logs/${VERSION}_step${MENU}_${signal}_signalEff.log
 done
