74105a520ed8fec458a0bad89c305e893d6bc2a4
Robert Ward <robert.james.ward@cern.ch>
2026-09-16 01:06:50 +0200

    Minimal scripts to test scaling and signal overrides

---
+++ scripts/testScalingsOverride.sh
@@ -0,0 +1,3 @@
+# time rate_table configs/V49nano_RelVal/rate_table/step1_cfg.yml --version V49nano_RelVal_MuonShower --scalings V49nano_AR25_170pre3
+# time rate_plots configs/V49nano_RelVal/rate_plots/muons.yaml --version V49nano_RelVal_MuonShower --scalings V49nano_AR25_170pre3
+time rate_plots configs/V49nano_RelVal/rate_plots/muons.yaml --version V49nano_RelVal_MuonShower
---
+++ scripts/testSignalOverride.sh
@@ -0,0 +1,3 @@
+time rate_table configs/V49nano_RelVal/rate_table/step1_cfg.yml --version V50nano_170pre3 --signal Hgg
+time rate_table configs/V49nano_RelVal/rate_table/step1_cfg.yml --version V50nano_170pre3 --signal HHTo2B2Tau
+time rate_table configs/V49nano_RelVal/rate_table/step1_cfg.yml --version V50nano_170pre3
