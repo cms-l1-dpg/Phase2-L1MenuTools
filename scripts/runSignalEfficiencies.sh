0877b52a74b967fcd70c52bdf389f48eccda63bb
Robert Ward <robert.james.ward@cern.ch>
2026-09-18 16:31:39 +0200

    Add more detailed output from time command to test efficiency gains

--- scripts/runSignalEfficiencies.sh
+++ scripts/runSignalEfficiencies.sh
@@ -1,11 +1,17 @@
-MENU=1p5
-# MENU=1
+# MENU=1p5
+MENU=1
 VERSION="V50nano_170pre3"
-
+REVISION=$1
+LOGDIR=${VERSION}_signalEff_$REVISION
 SIGNALS="DYLL_M50 Hgg HHTo4B HHTo2B2Tau TT VBFHToTauTau VBFHToBB VBFHToInv"
 
+mkdir logs/$LOGDIR
 for signal in $SIGNALS; do
-  { time rate_table configs/V50nano/rate_table/step${MENU}_cfg.yml \
+  { \time -v rate_table configs/V50nano/rate_table/step${MENU}_cfg.yml \
         --version $VERSION --signal $signal ; } \
-        |& tee logs/${VERSION}_step${MENU}_${signal}_signalEff.log
+        |& tee logs/$LOGDIR/${VERSION}_step${MENU}_${signal}_signalEff.log
 done
+
+   # { \time -v rate_table configs/V50nano/rate_table/step${MENU}_cfg.yml \
+   #      --version $VERSION ; } \
+   #      |& tee logs/$LOGDIR/${VERSION}_step${MENU}_minBias.log
