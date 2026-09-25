841bbfde3e636b396145f723d503c88417de2042
Robert Ward <robert.james.ward@cern.ch>
2026-09-25 16:36:46 +0200

    Extra tests for signal efficiencies

--- scripts/runSignalEfficiencies.sh
+++ scripts/runSignalEfficiencies.sh
@@ -1,9 +1,12 @@
 # MENU=1p5
 MENU=1
+# VERSION="V50nano_170pre3_140PU"
 VERSION="V50nano_170pre3"
 REVISION=$1
 LOGDIR=${VERSION}_signalEff_$REVISION
 SIGNALS="DYLL_M50 Hgg HHTo4B HHTo2B2Tau TT VBFHToTauTau VBFHToBB VBFHToInv"
+# SIGNALS="DYLL_M50 Hgg TT VBFHToTauTau"
+# SIGNALS="TT"
 
 mkdir logs/$LOGDIR
 for signal in $SIGNALS; do
@@ -12,6 +15,10 @@ for signal in $SIGNALS; do
         |& tee logs/$LOGDIR/${VERSION}_step${MENU}_${signal}_signalEff.log
 done
 
-   # { \time -v rate_table configs/V50nano/rate_table/step${MENU}_cfg.yml \
-   #      --version $VERSION ; } \
-   #      |& tee logs/$LOGDIR/${VERSION}_step${MENU}_minBias.log
+  # { MENU_TABLE_CHUNK=5000 \time -v rate_table configs/V50nano/rate_table/step${MENU}_cfg.yml \
+  #       --version $VERSION --signal $signal ; } \
+  #       |& tee logs/$LOGDIR/${VERSION}_step${MENU}_${signal}_chunk5k_signalEff.log
+  # { MENU_TABLE_CHUNK=2000 \time -v rate_table configs/V50nano/rate_table/step${MENU}_cfg.yml \
+  #       --version $VERSION --signal $signal ; } \
+  #       |& tee logs/$LOGDIR/${VERSION}_step${MENU}_${signal}_chunk2k_signalEff.log
+
